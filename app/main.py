c938bac79ac219107881367573242ee70f717399
octa-acostamonti <acostamonti.business@gmail.com>
2023-08-16 18:40:28 -0300

    app folder created

---
+++ app/main.py
@@ -0,0 +1,31 @@
+from fastapi import FastAPI
+from fastapi.params import Body
+from pydantic import BaseModel
+from typing import Optional
+
+
+app = FastAPI()
+
+class Post(BaseModel):
+    title: str
+    content: str
+    published: bool = True
+    rating: Optional[int] = None
+
+
+
+
+@app.get("/")
+async def root():
+    return  ({"mi no bia":"e e e "})
+
+
+@app.get("/proteinas/")
+def get_proteinas():
+    return {"manzana":"20gr"}
+
+@app.post("/createposts/")
+def create_posts(new_post: Post):
+    print(new_post)
+    return {"new_post":f"title: {new_post['title']} content: {new_post['content']}"}
+# title str, content str
