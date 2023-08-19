f5bb25fa3ac6aa5376cb4aa3ca6c2b8f154cc690
octa-acostamonti <acostamonti.business@gmail.com>
2023-08-19 16:48:14 -0300

    updated endpoints to use SQLAlchemy

--- app/main.py
+++ app/main.py
@@ -3,7 +3,7 @@ import psycopg2
 from psycopg2.extras import RealDictCursor
 from . import models
 from .database import engine, SessionLocal, get_db
-from sqlalchemy.orm import Session
+from sqlalchemy.orm import Session, load_only
 
 
 models.Base.metadata.create_all(bind=engine)
@@ -30,30 +30,47 @@ def test_conection(db: Session = Depends(get_db)):
     return {"data":productos}
 
 @app.get("/proteinas/")
-def get_proteinas():
-    cursor.execute("""SELECT producto,marca,proteina_g,cantidad FROM productos """)
-    proteina = cursor.fetchall()
+def get_proteinas(db: Session = Depends(get_db)):
+    
+    # cursor.execute("""SELECT producto,marca,proteina_g,cantidad FROM productos """)
+    # proteina = cursor.fetchall()
+    
+    proteina = db.query(models.Productos).options(load_only("producto", "marca", "proteina_g", "cantidad")).all()
+    
     return {"proteinas":proteina}
 
 
 @app.get("/grasas/")
-def get_grasas():
-    cursor.execute("""SELECT producto,marca,grasa_g,cantidad FROM productos """)
-    grasa = cursor.fetchall()
+def get_grasas(db: Session = Depends(get_db)):
+    
+    # cursor.execute("""SELECT producto,marca,grasa_g,cantidad FROM productos """)
+    # grasa = cursor.fetchall()
+    
+    grasa = db.query(models.Productos).options(load_only("producto", "marca", "grasa_g", "cantidad")).all()
+    
     return {"grasas":grasa}
 
 
 @app.get("/carbohidratos/")
-def get_carbohidratos():
-    cursor.execute("""SELECT producto,marca,carbohidrato_g,cantidad FROM productos """)
-    carbohidrato = cursor.fetchall()
-    return {"carbohidratos":carbohidrato}
+def get_carbohidratos(db: Session = Depends(get_db)):
+    
+    # cursor.execute("""SELECT producto,marca,carbohidrato_g,cantidad FROM productos """)
+    # carbohidrato = cursor.fetchall()
+    
+    carbohidratos = db.query(models.Productos).options(load_only("producto", "marca", "carbohidrato_g", "cantidad")).all()
+    
+    return {"carbohidratos":carbohidratos}
 
 
 @app.get("/productos/")
-def get_productos():
-    cursor.execute("""SELECT * FROM productos""")
-    producto = cursor.fetchall()
-    return {"productos":producto}
+def get_productos(db: Session = Depends(get_db)):
+    
+    # cursor.execute("""SELECT * FROM productos""")
+    # producto = cursor.fetchall()
+    
+    productos = db.query(models.Productos).all()
+    
+    return {"data":productos}
+
 
 
