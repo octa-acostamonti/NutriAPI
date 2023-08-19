7a8d5e59dc0c253a8c868eb452e83cede40ee2c7
octa-acostamonti <acostamonti.business@gmail.com>
2023-08-19 14:09:22 -0300

    modified main.py added 5 endpoints

--- app/main.py
+++ app/main.py
@@ -1,32 +1,58 @@
-from fastapi import FastAPI
-from fastapi.params import Body
-from pydantic import BaseModel
-from typing import Optional
+from fastapi import FastAPI, Depends
+import psycopg2
+from psycopg2.extras import RealDictCursor
+from . import models
+from .database import engine, SessionLocal, get_db
+from sqlalchemy.orm import Session
 
+
+models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
 
-class Post(BaseModel):
-    title: str
-    content: str
-    published: bool = True
-    rating: Optional[int] = None
-
-
+try:
+    conn = psycopg2.connect(host="localhost",database="NutriAPI",user="postgres",
+    password="root",cursor_factory=RealDictCursor)
+    cursor = conn.cursor()
+    print("Database conection was succesfull!")
+except Exception as e:
+    print("Conection to database failed")
+    print("The Error was: ",e)
 
 
 @app.get("/")
 async def root():
     return  ({"mi no bia":"e e e "})
 
+@app.get("/sqlalchemy")
+def test_conection(db: Session = Depends(get_db)):
+    return {"status":"success"}
 
 @app.get("/proteinas/")
 def get_proteinas():
-    return {"manzana":"20gr"}
+    cursor.execute("""SELECT producto,marca,proteina_g,cantidad FROM productos """)
+    proteina = cursor.fetchall()
+    return {"proteinas":proteina}
 
-@app.post("/createposts/")
-def create_posts(new_post: Post):
-    print(new_post)
-    print({"new_post":f"title: {new_post.title} content: {new_post.content}"})
-    return {"new_post":f"title: {new_post.title} content: {new_post.content}"}
-# title str, content str
+
+@app.get("/grasas/")
+def get_grasas():
+    cursor.execute("""SELECT producto,marca,grasa_g,cantidad FROM productos """)
+    grasa = cursor.fetchall()
+    return {"grasas":grasa}
+
+
+@app.get("/carbohidratos/")
+def get_carbohidratos():
+    cursor.execute("""SELECT producto,marca,carbohidrato_g,cantidad FROM productos """)
+    carbohidrato = cursor.fetchall()
+    return {"carbohidratos":carbohidrato}
+
+
+@app.get("/productos/")
+def get_productos():
+    cursor.execute("""SELECT * FROM productos""")
+    producto = cursor.fetchall()
+    return {"productos":producto}
+
+
