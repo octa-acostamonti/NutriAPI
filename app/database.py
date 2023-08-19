b4b71ecb3bcc7de594c466e56ac62af3bdcecc6e
octa-acostamonti <acostamonti.business@gmail.com>
2023-08-19 16:17:45 -0300

    solved a bug related to the SessionLocal object

--- app/database.py
+++ app/database.py
@@ -11,7 +11,7 @@ SessionLocal = sessionmaker(autocommit=False,autoflush=False,bind=engine)
 Base = declarative_base()
 
 def get_db():
-    db = SessionLocal
+    db = SessionLocal()
     try:
         yield db 
     finally:
