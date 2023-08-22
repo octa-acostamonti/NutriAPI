4ad8b91be82bf5de1777f2330db3cf52ce71cb22
Octavio Acosta Monti <acostamonti.business@gmail.com>
2023-08-22 14:56:39 -0300

    added comments to schemas.py

--- app/schemas.py
+++ app/schemas.py
@@ -2,6 +2,7 @@ from pydantic import BaseModel
 
 
 class Response(BaseModel):
+    """ Configuracion de la respuesta base """
     producto: str
     marca: str
     cantidad: str
@@ -10,15 +11,19 @@ class Response(BaseModel):
 
 
 class ResponseProteina(Response):
+    """ Configuracion de la respuesta de proteinas con herencia de la respuesta base """
     proteina_g: float
 
 class ResponseGrasa(Response):
+    """ Configuracion de la respuesta de grasas con herencia de la respuesta base """
     grasa_g: float
 
 class ResponseCarbohidratos(Response):
+    """ Configuracion de la respuesta de carbohidratos con herencia de la respuesta base """
     carbohidrato_g: float
 
 class ResponseProductos(Response):
+    """ Configuracion de la respuesta de los productos con herencia de la respuesta base """
     id:int
     proteina_g: float
     grasa_g: float
