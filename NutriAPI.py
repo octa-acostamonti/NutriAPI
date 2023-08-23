fe883f46ec60510a425aad717333c6078ca0e110
Octavio Acosta Monti <acostamonti.business@gmail.com>
2023-08-23 13:30:01 -0300

    implemented a better logic and comments for data apending to dataframe

--- NutriAPI.py
+++ NutriAPI.py
@@ -1,25 +1,29 @@
 import requests
+import time
 import pandas as pd
 from bs4 import BeautifulSoup
 
 
-URLS = []
+def conseguir_urls():
+    """ Conseguir las URLS de la página fatsecret.com.ar y almacenarlas en una lista """
+    
+    URLS = []
+    
+    URL_BASE = "https://www.fatsecret.com.ar/calor%C3%ADas-nutrici%C3%B3n/search?q=9+de+Oro&pg=" 
 
-URL_BASE = "https://www.fatsecret.com.ar/calor%C3%ADas-nutrici%C3%B3n/search?q=9+de+Oro&pg=" 
+    for num in range(0,271):
 
-
-for num in range(0,271):
-
-    URL = URL_BASE + str(num) 
-    URLS.append(URL) 
-
+        URL = URL_BASE + str(num) 
+        URLS.append(URL) 
+    return URLS
 
 Tabla_Nutricional_Productos = pd.DataFrame()
 
 
 def main():
     global Tabla_Nutricional_Productos
-    
+    time_start = time.time()
+    URLS = conseguir_urls()
     for url in URLS:
     
         page = requests.get(url)
@@ -65,25 +69,27 @@ def main():
         Carbohidrato = [c.replace(",", ".").replace("g", "") for c in Carbohidrato]
         Caloria = [c.replace("kcal", "") for c in Caloria]
         Cantidad = [c.split('(')[-1].replace(")", "").replace("g", "") for c in Cantidad]
+        
 
-
-        if not Tabla_Nutricional_Productos.empty:
+        if Tabla_Nutricional_Productos.empty:
+        # Si no existe; Crear el DataFrame con las columnas pertinentes.
             columnas = ["Producto","Marca","Cantidad(g)","Caloria(kcal)","Grasa(g)","Carbohidrato(g)","Proteina(g)"]
             data_ = list(zip(Producto, Marca, Cantidad, Caloria, Grasa, Carbohidrato, Proteina))
-            df = pd.DataFrame(columns=columnas,data=data_)
-            Tabla_Nutricional_Productos = pd.concat([Tabla_Nutricional_Productos,df],ignore_index=True)
+            Tabla_Nutricional_Productos = pd.DataFrame(columns=columnas, data=data_)
+            print("Creado el DataFrame")
         else:
-            columnas = ["Producto","Marca","Cantidad(g)","Caloria(kcal)","Grasa(g)","Carbohidrato(g)","Proteina(g)"]
-            data_ = list(zip(Producto, Marca, Cantidad, Caloria, Grasa, Carbohidrato, Proteina))
-            Tabla_Nutricional_Productos = pd.DataFrame(columns=columnas,data=data_)
+        # Una vez existe; Ingestar la data al DataFrame ya creado.
+            new_data = list(zip(Producto, Marca, Cantidad, Caloria, Grasa, Carbohidrato, Proteina))
+            new_df = pd.DataFrame(columns=Tabla_Nutricional_Productos.columns, data=new_data)
+            Tabla_Nutricional_Productos = pd.concat([Tabla_Nutricional_Productos, new_df], ignore_index=True)
+            print("Data Ingestada")
 
-    
-    
     Tabla_Nutricional_Productos["Caloria(kcal)"] = Tabla_Nutricional_Productos["Caloria(kcal)"].astype("Int64")
     Tabla_Nutricional_Productos["Grasa(g)"] = Tabla_Nutricional_Productos["Grasa(g)"].astype("Float64")
     Tabla_Nutricional_Productos["Carbohidrato(g)"] = Tabla_Nutricional_Productos["Carbohidrato(g)"].astype("Float64")
     Tabla_Nutricional_Productos["Proteina(g)"] = Tabla_Nutricional_Productos["Proteina(g)"].astype("Float64")
-
+    time_elapsed = time.time() - time_start
+    print(f"Time elapsed was:{time_elapsed:.2f}")
     return Tabla_Nutricional_Productos.to_csv("Tabla_Nutricional_Productos.csv")
 
 
