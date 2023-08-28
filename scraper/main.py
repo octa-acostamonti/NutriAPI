c22fc8c78764afecab4009cc187aa5038bff6c22
octa-acostamonti <acostamonti.business@gmail.com>
2023-08-28 20:10:11 -0300

    added coments to 'main.py'

--- scraper/main.py
+++ scraper/main.py
@@ -2,11 +2,16 @@ from carga import carga
 from extraer import extraer_info_nutricional
 from utils import conseguir_urls
 
+
+
 def main():
+    """ Main loop for the scraper """
     
-    URLS = conseguir_urls()
-    info_extraeida = extraer_info_nutricional(URLS)
-    carga("productos",info_extraeida)
+    URLS = conseguir_urls() # Gets a list of all the URLS to scrape
+
+    Tabla_Nutricional_Productos = extraer_info_nutricional(URLS) # Scrapes the URLS and gets the Nutritional Information and stores it into a Pandas DataFrame
+    
+    carga("productos",Tabla_Nutricional_Productos) # Creates a conection with the table "productos" in PostreSQL, and loads the Pandas DataFrame
 
 
 if __name__ == "__main__":
