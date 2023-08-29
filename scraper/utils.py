e2ca1ad6d6a43efda9c211f3a3e257e85543e845
octa-acostamonti <acostamonti.business@gmail.com>
2023-08-28 22:59:38 -0300

    found a new limit, more products added 2756

--- scraper/utils.py
+++ scraper/utils.py
@@ -5,7 +5,7 @@ def conseguir_urls():
     
     URL_BASE = "https://www.fatsecret.com.ar/calor%C3%ADas-nutrici%C3%B3n/search?q=9+de+Oro&pg=" 
 
-    for num in range(0,271): # '271' es el numero maximo de páginas de la URL BASE
+    for num in range(0,276): # '276' es el numero maximo de páginas de la URL BASE
 
         URL = URL_BASE + str(num) 
         URLS.append(URL) 
