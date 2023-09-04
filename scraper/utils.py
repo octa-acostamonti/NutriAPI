49c031a093312eb084fff1dcc144be9877401322
octa-acostamonti <acostamonti.business@gmail.com>
2023-09-04 15:00:46 -0300

    modified to english the comments

--- scraper/utils.py
+++ scraper/utils.py
@@ -1,11 +1,11 @@
 def conseguir_urls():
-    """ Conseguir las URLS de la página fatsecret.com.ar y almacenarlas en una lista """
+    """ Get all of the pages of the page fatsecret.com.ar and append them into a list """
     
     URLS = []
     
     URL_BASE = "https://www.fatsecret.com.ar/calor%C3%ADas-nutrici%C3%B3n/search?q=9+de+Oro&pg=" 
 
-    for num in range(0,276): # '276' es el numero maximo de páginas de la URL BASE
+    for num in range(0,276): # '276' is the max page on fatsecret.com.ar
 
         URL = URL_BASE + str(num) 
         URLS.append(URL) 
