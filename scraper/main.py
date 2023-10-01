68dcb822ed0fab3dad62ae94123b6173d0def062
octa-acostamonti <acostamonti.business@gmail.com>
2023-10-01 01:55:18 -0300

    a

--- scraper/main.py
+++ scraper/main.py
@@ -1,18 +1,28 @@
 from carga import carga
 from extraer import extraer_info_nutricional
 from utils import conseguir_urls
-
+from cProfile import Profile
 
 
 
 def main():
     """ Main loop for the scraper """
-    
-    URLS = conseguir_urls() # Gets a list of all the URLS to scrape
 
-    Tabla_Nutricional_Productos = extraer_info_nutricional(URLS) # Scrapes the URLS and gets the Nutritional Information and stores it into a Pandas DataFrame
+    import pstats
+
+    with Profile() as pr:
+        
+
     
-    carga("productos",Tabla_Nutricional_Productos) # Creates a conection with the table "productos" in PostreSQL, and loads the Pandas DataFrame
+        URLS = conseguir_urls() # Gets a list of all the URLS to scrape
+
+        Tabla_Nutricional_Productos = extraer_info_nutricional(URLS) # Scrapes the URLS and gets the Nutritional Information and stores it into a Pandas DataFrame
+    
+        carga("productos",Tabla_Nutricional_Productos) # Creates a conection with the table "productos" in PostreSQL, and loads the Pandas DataFrame
+
+    stats = pstats.Stats(pr)
+    stats.sort_stats(pstats.SortKey.TIME)
+    stats.dump_stats(filename='stats.prof')
 
 
 if __name__ == "__main__":
